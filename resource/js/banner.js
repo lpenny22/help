ecb21806633554e63cd57e6917b9c950c3590a8a
lpenny22 <lpenny_2002@yahoo.com>
2018-02-14 15:40:51 -0600

    Update banner.js

--- resource/js/banner.js
+++ resource/js/banner.js
@@ -1,6 +1,6 @@
 // JScript source code
 var bannertext = '<div style="margin:0 auto; width:700px;position: relative; top: 0px; left: 0px; padding-left: 0px; background-color: #000000">'
-bannertext += '\n<img src="../resource/images/dxcuniversityhelpOrig2.jpg">'
+bannertext += '\n<img src="https://raw.githubusercontent.com/lpenny22/help/master/resource/images/dxcuniversityhelpOrig2.jpg">'
 bannertext += '\n</div> '
 document.write(bannertext);
 
