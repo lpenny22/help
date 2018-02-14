36e51a236b2b4a52d6c3244b7b32dc82c67444bb
lpenny22 <lpenny_2002@yahoo.com>
2018-02-14 12:01:32 -0600

    Add files via upload

--- resource/js/banner.js
+++ resource/js/banner.js
@@ -1,6 +1,6 @@
 // JScript source code
 var bannertext = '<div style="margin:0 auto; width:700px;position: relative; top: 0px; left: 0px; padding-left: 0px; background-color: #000000">'
-bannertext += '\n<img src="../resource/images/DXCUniversityBannerPhotoLogo.png">'
+bannertext += '\n<img src="../resource/images/dxc_university_helpOrig2.png">'
 bannertext += '\n</div> '
 document.write(bannertext);
 
